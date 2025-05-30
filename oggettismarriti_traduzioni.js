aa729c81422c7cb89227004a7eb5c51eb1ae9cc3
Pierluigi Santin <p.santin@e-deimos.it>
2025-05-30 16:13:49 +0000

    sono arrivato agli oggetti smarriti

---
+++ oggettismarriti_traduzioni.js
@@ -0,0 +1,32 @@
+
+     const resources = {
+  it: {
+    translation: {
+      labels: {
+   
+        ultimo:""
+      },
+      tooltips: {
+       
+        ultimo:""
+      }
+    }
+  },
+  en: {
+    translation: {
+      labels: {
+        
+
+        ultimo:""
+
+      },
+      tooltips: {
+       
+        ultimo:""
+
+
+
+      }
+    }
+  }
+};
