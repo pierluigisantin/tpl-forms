b8661ecdda2404abbd9d76303f645bd1d7fb2d7d
Pierluigi Santin <p.santin@e-deimos.it>
2025-05-31 13:41:07 +0000

    un po di oggetti smarriti

--- oggettismarriti_traduzioni.js
+++ oggettismarriti_traduzioni.js
@@ -3,7 +3,25 @@
   it: {
     translation: {
       labels: {
-   
+        legendanagrafica:"DATI ANAGRAFICI",
+        nome:"Nome",
+        cognome:"Cognome",
+        nazione:"Nazionalità",
+        comune:"Comune",
+        indirizzo:"Indirizzo",
+        mail:"E-mail",
+        prefissotelefonico:"Prefisso telefonico internazionale",
+        numerotelefonico:"Numero di cellulare",
+        legendainfoevento:"DOVE PRESUMI CHE SIA AVVENUTO LO SMARRIMENTO",
+        accounttpl:"Azienda di riferimento",
+        servizio:"Tipo di servizio",
+        dataoraevento:"Data e ora presunta dello smarrimento",
+        comuneevento:"Comune dove lo smarrimento si è presumibilmente verificato",
+        linea:"Linea dove lo smarrimento si è presumibilmente verificato",
+        direzione:"Direzione (indicare il capolinea o l’approdo di destinazione)",
+        descrizione:"Descrizione dell'oggetto smarrito",
+        introdescrizione:"Descrivi l'oggetto smarrito in modo dettagliato. Indica, se possibile, il colore, eventuali scritte o nomi presenti, il materiale o altre caratteristiche distintive. Esempi di oggetti smarriti possono essere: zaini, borse, cartelle da disegno, libri, cellulari, portafogli, chiavi, occhiali, indumenti, biglietti o abbonamenti. Una descrizione precisa aiuta a facilitare l’identificazione dell’oggetto. Se desideri allegare immagini o altri documenti utili, potrai farlo rispondendo direttamente alla e-mail di notifica che riceverai al termine della compilazione di questo form",
+        invia:"Invia segnalazione",
         ultimo:""
       },
       tooltips: {
