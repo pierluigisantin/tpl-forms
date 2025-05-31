9dda41fad1d23652f6a2896af47379b5452d8904
Pierluigi Santin <p.santin@e-deimos.it>
2025-05-31 13:58:10 +0000

    1 versione semifunzionante di oggetti smarriti

--- oggettismarriti_traduzioni.js
+++ oggettismarriti_traduzioni.js
@@ -1,46 +1,55 @@
 
-     const resources = {
+const resources = {
   it: {
     translation: {
       labels: {
-        legendanagrafica:"DATI ANAGRAFICI",
-        nome:"Nome",
-        cognome:"Cognome",
-        nazione:"Nazionalità",
-        comune:"Comune",
-        indirizzo:"Indirizzo",
-        mail:"E-mail",
-        prefissotelefonico:"Prefisso telefonico internazionale",
-        numerotelefonico:"Numero di cellulare",
-        legendainfoevento:"DOVE PRESUMI CHE SIA AVVENUTO LO SMARRIMENTO",
-        accounttpl:"Azienda di riferimento",
-        servizio:"Tipo di servizio",
-        dataoraevento:"Data e ora presunta dello smarrimento",
-        comuneevento:"Comune dove lo smarrimento si è presumibilmente verificato",
-        linea:"Linea dove lo smarrimento si è presumibilmente verificato",
-        direzione:"Direzione (indicare il capolinea o l’approdo di destinazione)",
-        descrizione:"Descrizione dell'oggetto smarrito",
-        introdescrizione:"Descrivi l'oggetto smarrito in modo dettagliato. Indica, se possibile, il colore, eventuali scritte o nomi presenti, il materiale o altre caratteristiche distintive. Esempi di oggetti smarriti possono essere: zaini, borse, cartelle da disegno, libri, cellulari, portafogli, chiavi, occhiali, indumenti, biglietti o abbonamenti. Una descrizione precisa aiuta a facilitare l’identificazione dell’oggetto. Se desideri allegare immagini o altri documenti utili, potrai farlo rispondendo direttamente alla e-mail di notifica che riceverai al termine della compilazione di questo form",
-        invia:"Invia segnalazione",
-        ultimo:""
+        erroreCampoObbligatorio: "Devi compilare questo campo",
+        seleziona: "--Seleziona--",
+        legendanagrafica: "DATI ANAGRAFICI",
+        nome: "Nome",
+        cognome: "Cognome",
+        nazione: "Nazionalità",
+        comune: "Comune",
+        indirizzo: "Indirizzo",
+        citta: "Città",
+        localita: "Località",
+        cap: "CAP",
+        provincia: "Provincia",
+        mail: "E-mail",
+        prefissotelefonico: "Prefisso telefonico internazionale",
+        numerotelefonico: "Numero di cellulare",
+        legendainfoevento: "DOVE PRESUMI CHE SIA AVVENUTO LO SMARRIMENTO",
+        accountTpl: "Azienda di riferimento",
+        servizio: "Tipo di servizio",
+        dataoraevento: "Data e ora presunta dello smarrimento",
+        comuneevento: "Comune dove lo smarrimento si è presumibilmente verificato",
+        linea: "Linea dove lo smarrimento si è presumibilmente verificato",
+        direzione: "Direzione (indicare il capolinea o l’approdo di destinazione)",
+        descrizione: "Descrizione dell'oggetto smarrito",
+        introdescrizione: "Descrivi l'oggetto smarrito in modo dettagliato. Indica, se possibile, il colore, eventuali scritte o nomi presenti, il materiale o altre caratteristiche distintive. Esempi di oggetti smarriti possono essere: zaini, borse, cartelle da disegno, libri, cellulari, portafogli, chiavi, occhiali, indumenti, biglietti o abbonamenti. Una descrizione precisa aiuta a facilitare l’identificazione dell’oggetto. Se desideri allegare immagini o altri documenti utili, potrai farlo rispondendo direttamente alla e-mail di notifica che riceverai al termine della compilazione di questo form",
+        invia: "Invia segnalazione",
+        ultimo: ""
       },
       tooltips: {
-       
-        ultimo:""
+        accountTpl: ": Se possibile, indica l’azienda a cui compete il servizio dove presumi che sia avvenuto lo smarrimento. Se non sai quale azienda indicare, scegli l’opzione TPL FVG",
+        servizio:"Seleziona il servizio dove presumi che lo smarrimento sia avvenuto. Se non sai quale servizio indicare, non compilare questo campo",
+        dataoraevento:"Anche se non sei certo di quando lo smarrimento sia avvenuto, indica una data e un’ora presunte",
+        linea:"Questo campo si attiva dopo aver selezionato l’azienda di riferimento. Le linee proposte dipendono dall’azienda selezionata. Se come azienda di riferimento hai selezionato TPL FVG, l’elenco includerà tutte le linee operate da Tpl Fvg in Friuli Venezia Giulia e nelle aree confinanti. Se non sai su quale linea sia avvenuto lo smarrimento, non compilare questo campo",
+        ultimo: ""
       }
     }
   },
   en: {
     translation: {
       labels: {
-        
 
-        ultimo:""
+
+        ultimo: ""
 
       },
       tooltips: {
-       
-        ultimo:""
+
+        ultimo: ""
 
 
 
