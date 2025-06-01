399d3d7742539a1dfc0e4f240e29e5882bf5b30c
Pierluigi Santin <p.santin@e-deimos.it>
2025-06-01 17:32:32 +0000

    iniziato richiesta rimborso

---
+++ rimborso_traduzioni.js
@@ -0,0 +1,36 @@
+
+const resources = {
+  it: {
+    translation: {
+      labels: {
+        erroreCampoObbligatorio: "Devi compilare questo campo",
+        seleziona: "--Seleziona--",
+
+        ultimo: ""
+      },
+      tooltips: {
+
+        ultimo: ""
+      }
+    }
+  },
+  en: {
+    translation: {
+      labels: {
+        erroreCampoObbligatorio: "You must fill this field",
+          seleziona: "--Select--",
+         
+
+        ultimo: ""
+
+      },
+      tooltips: {
+
+        ultimo: ""
+
+
+
+      }
+    }
+  }
+};
