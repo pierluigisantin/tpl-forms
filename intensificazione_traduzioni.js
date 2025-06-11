ed2c271ec9995721937117dace1d715a649ca53d
Pierluigi Santin <p.santin@e-deimos.it>
2025-06-11 17:47:41 +0000

    primo intensificazione

---
+++ intensificazione_traduzioni.js
@@ -0,0 +1,40 @@
+
+     const resources = {
+  it: {
+    translation: {
+      labels: {
+
+        erroreCampoObbligatorio: "Devi compilare questo campo",
+        seleziona: "--Seleziona--",
+       
+        valoreNonValidoDatalist: "Devi selezionare un valore dalla lista",
+       
+        ultimo:""
+      },
+      tooltips: {
+        
+        ultimo:""
+      }
+    }
+  },
+  en: {
+    translation: {
+      labels: {
+        erroreCampoObbligatorio: "You must fill this field",
+        seleziona: "--Select--",
+        codiceFiscaleNonValido:"The fiscal/tax code entered is not valid",
+        valoreNonValidoDatalist: "You must select a value from list",
+        
+        ultimo:""
+
+      },
+      tooltips: {
+        
+        ultimo:""
+
+
+
+      }
+    }
+  }
+};
