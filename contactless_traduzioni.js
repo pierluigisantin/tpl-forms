7ff335a3c78b09339f2b8ee508bac4f315ae5cb4
Pierluigi Santin <p.santin@e-deimos.it>
2025-06-15 15:03:08 +0000

    intensificazione e contactless

--- contactless_traduzioni.js
+++ contactless_traduzioni.js
@@ -6,7 +6,7 @@ const resources = {
     translation: {
       labels: {
 
-        legendanagrafica: "DATI ANAGRAFICI DELLA PERSONA CHE PRESENTA IL RECLAMO",
+        legendanagrafica: "DATI ANAGRAFICI DELLA PERSONA CHE HA RILEVATO IL PROBLEMA",
         valoreNonValidoDatalist: "Devi selezionare un valore valido dalla lista",
         erroreCampoObbligatorio: "Devi compilare questo campo",
         seleziona: "--Seleziona--",
@@ -19,12 +19,17 @@ const resources = {
         nazione: "Nazione di residenza",
         ncivico: "Street N°",
         indirizzo: "Indirizzo di residenza o domicilio",
-
+        localita: "Località",
+        citta: "Città",
         comune: "Comune",
-
+        cap: "CAP",
+        provincia: "Provincia o distretto",
         prefissotelefonico: "Prefisso telefonico internazionale",
         numerotelefonico: "Numero di cellulare",
         mail: "E-mail",
+        email_confirm_label: "Conferma e-mail",
+        email_mismatch: "le mail non corrispondono",
+        legendainfoevento: "Problema rilevato",
         appcontactless: "Il problema che hai rilevato riguarda",
         appcontactlessOptions: ["--Seleziona--", "L’acquisto di biglietti tramite dispositivi contactless con carta di credito o carta di debito (bancomat)",
           "La convalida di biglietti cartacei con QR code tramite lettura ottica a bordo degli autobus",
@@ -33,20 +38,20 @@ const resources = {
         ],
         appcontactlessOptionsShowHide: {
           "L’acquisto di biglietti tramite dispositivi contactless con carta di credito o carta di debito (bancomat)": {
-            show: ["circuitocarta", "accountTpl", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet", "dataoraevento", "linea", "nvettura", "descrizione"],
-            hide: ["tipoluogoproblemacontactless","numerotelefonicoapp","indirizzomailapp"]
+            show: ["circuitocarta", "accountTpl", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet", "dataoraevento", "linea", "nvettura", "descrizione", "tipoproblemacontactless"],
+            hide: ["numerotelefonicoapp", "indirizzomailapp"]
           },
           "La convalida di biglietti cartacei con QR code tramite lettura ottica a bordo degli autobus": {
-            show: [ "accountTpl", , "dataoraevento", "linea", "nvettura", "descrizione", "tipoluogoproblemacontactless"],
-            hide: ["circuitocarta", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet","numerotelefonicoapp","indirizzomailapp"]
+            show: ["accountTpl", , "dataoraevento", "linea", "nvettura", "descrizione", "tipoproblemacontactless"],
+            hide: ["circuitocarta", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet", "numerotelefonicoapp", "indirizzomailapp"]
           },
           "L’app Tpl Fvg": {
-            show: ["accountTpl", , "dataoraevento", "linea", "nvettura", "descrizione","numerotelefonicoapp","indirizzomailapp"],
+            show: ["accountTpl", , "dataoraevento", "linea", "nvettura", "descrizione", "numerotelefonicoapp", "indirizzomailapp", "tipoproblemacontactless"],
             hide: ["circuitocarta", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet"]
           },
           "L’app Glimble": {
-            show: ["accountTpl", , "dataoraevento",  "descrizione","numerotelefonicoapp","indirizzomailapp"],
-              hide: ["circuitocarta", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet","linea", "nvettura","tipoproblemacontactless"]
+            show: ["accountTpl", , "dataoraevento", "descrizione", "numerotelefonicoapp", "indirizzomailapp"],
+            hide: ["circuitocarta", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet", "linea", "nvettura", "tipoproblemacontactless"]
           }
         },
         tipoproblemacontactless: "Indica il problema che hai rilevato",
@@ -86,15 +91,34 @@ const resources = {
         dataoraevento: "Data e ora dell'evento",
         linea: "Linea urbana dove il problema si è verificato o dove è stato rilevato",
         nvettura: "Numero di matricola dell’autobus",
+        introdescrizione: "Descrivi il problema in modo sintetico ma completo. Specifica, se possibile, ciò che ti aspettavi che accadesse e cosa invece è successo ",
         descrizione: "Descrizione del problema ",
         tipoluogoproblemacontactless: "Il problema si è verificato su un solo autobus o si verifica sempre? ",
         tipoluogoproblemacontactlessOptions: ["--Seleziona--", "Su un solo autobus", "Sempre"],
-        indirizzomailapp: "Indirizzo e-mail associato al profilo nell’app Tpl Fvg",
+        indirizzomailapp: "Indirizzo e-mail associato al profilo nell’app ",
         numerotelefonicoapp: "Numero di telefono associato al profilo",
+
+
+        consensoobbligatorio: "CONSENSO OBBLIGATORIO",
+        privacy: "Dichiaro di aver letto e compreso l’informativa sulla privacy di Tpl Fvg, disponibile sul sito www.tplfvg.it e accessibile tramite il link riportato qui sotto",
+        consensofacoltativo: "CONSENSI FACOLTATIVI",
+        indaginidemoscopiche: "Acconsento a essere contattato da Tpl Fvg, anche tramite e-mail, per partecipare a indagini demoscopiche o ricerche di mercato sul livello di soddisfazione e sull’utilizzo dei servizi di trasporto pubblico locale",
+        attivitacommerciali: "Acconsento a ricevere comunicazioni relative a offerte commerciali, promozioni, agevolazioni o sconti su prodotti, servizi e attività di Tpl Fvg o delle aziende consorziate",
+        analisipreferenze: "Acconsento all’analisi delle mie preferenze e abitudini di utilizzo del trasporto pubblico locale, anche mediante elaborazioni elettroniche, al fine di migliorare l’offerta, soddisfare specifiche esigenze, orientare la pianificazione del servizio e ricevere comunicazioni personalizzate da parte di Tpl Fvg o delle aziende consorziate",
+        comunicazionepartner: "Acconsento a ricevere comunicazioni pubblicitarie, promozioni e informazioni commerciali relative a prodotti, servizi e attività di soggetti terzi, partner di Tpl Fvg o delle aziende consorziate",
+        cessioneterzi: "Acconsento alla comunicazione dei miei dati personali a soggetti terzi per finalità commerciali e di marketing",
+
+
         ultimo: ""
       },
       tooltips: {
-
+        accountTpl: "Indica l’azienda sul cui servizio urbano hai rilevato il problema. Se non sai quale azienda indicare, scegli l’opzione TPL FVG",
+        panintro: "Il PAN (Primary Account Number) è il numero da 13 a 19 cifre stampato sulla tua carta di pagamento. Le prime 4 e le ultime 4 cifre servono a identificare la transazione",
+        panvirtualeintro: "Se hai virtualizzato la tua carta in un wallet, entra nel wallet per visualizzare il PAN. Il PAN del wallet ha solo 4 cifre, sempre diverse da quelle della carta fisica",
+        dataoraevento: "Indica la data e l’ora dell’operazione o del momento in cui hai rilevato il problema. Se non conosci l’orario esatto, indica quello approssimativo o il momento in cui ti sei accorto del problema",
+        linea: "Questo campo si attiva dopo aver selezionato l’azienda di riferimento, da cui dipendono le linee proposte. Se come azienda di riferimento hai selezionato TPL FVG, l’elenco includerà tutte le linee urbane operate da Tpl Fvg. Se la segnalazione o l’evento non sono collegati a una linea, puoi lasciare questo campo in bianco",
+        nvettura: "Il numero di matricola dell’autobus è un codice identificativo composto da tre o quattro cifre, generalmente visibile sia all’esterno sia all’interno del mezzo. Il numero di matricola non coincide mai con il numero di linea",
+        numerotelefonicoapp: "Inserisci il numero di telefono che hai usato per attivare l’app o su cui l’app è installata, senza indicare il prefisso internazionale",
         ultimo: ""
       }
     }
@@ -102,13 +126,123 @@ const resources = {
   en: {
     translation: {
       labels: {
+        legendanagrafica: "PERSONAL DATA OF THE INDIVIDUAL WHO REPORTED THE ISSUE",
+        valoreNonValidoDatalist: "Please select a valid value from the list",
+        erroreCampoObbligatorio: "This field is required",
+        seleziona: "--Select--",
 
+        tipoSoggetto: "Who is submitting the complaint",
+        tipoSoggettoOptions: ["--Select--", "Private individual", "Company", "Public entity", "Association"],
+        ragioneSociale: "Name of association, public entity or company",
+        nome: "First name",
+        cognome: "Last name",
+        nazione: "Country of residence",
+        ncivico: "Street number",
+        indirizzo: "Address (residence or domicile)",
+        localita: "Locality",
+        citta: "City",
+        comune: "Municipality",
+        cap: "Postal code",
+        provincia: "Province or district",
+        prefissotelefonico: "International phone prefix",
+        numerotelefonico: "Mobile phone number",
+        mail: "E‑mail",
+        email_confirm_label: "Confirm e‑mail",
+        email_mismatch: "E‑mails do not match",
+
+        legendainfoevento: "Reported issue",
+        appcontactless: "The issue you reported relates to",
+        appcontactlessOptions: ["--Select--",
+          "Purchasing tickets via contactless payment (credit/debit card)",
+          "Validating paper tickets with QR code on buses",
+          "Tpl Fvg mobile app",
+          "Glimble mobile app"
+        ],
+        tipoproblemacontactless: "Specify the issue you encountered",
+        tipoproblemacontactlessOptions: {
+          "--Select--": [],
+          "Purchasing tickets via contactless payment (credit/debit card)": [
+            "Credit/debit card was blocked",
+            "Charged amount does not match purchase",
+            "I purchased ticket(s) but charges did not appear on card",
+            "Charges appeared on different day than travel",
+            "Other"
+          ],
+          "Validating paper tickets with QR code on buses": [
+            "Unable to validate ticket",
+            "A ticket appears validated though I never used it",
+            "Other"
+          ],
+          "Tpl Fvg mobile app": [
+            "I bought ticket(s) that did not appear",
+            "Unable to access my profile",
+            "Unable to validate purchased tickets",
+            "Subscription not visible in the app",
+            "Other"
+          ],
+          "Glimble mobile app": []
+        },
+        appcontactlessOptionsShowHide: {
+          "Purchasing tickets via contactless payment (credit/debit card)": {
+            show: ["circuitocarta", "accountTpl", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet", "dataoraevento", "linea", "nvettura", "descrizione", "tipoproblemacontactless"],
+            hide: ["numerotelefonicoapp", "indirizzomailapp"]
+          },
+          "Validating paper tickets with QR code on buses": {
+            show: ["accountTpl", , "dataoraevento", "linea", "nvettura", "descrizione", "tipoproblemacontactless"],
+            hide: ["circuitocarta", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet", "numerotelefonicoapp", "indirizzomailapp"]
+          },
+          "Tpl Fvg mobile app": {
+            show: ["accountTpl", , "dataoraevento", "linea", "nvettura", "descrizione", "numerotelefonicoapp", "indirizzomailapp", "tipoproblemacontactless"],
+            hide: ["circuitocarta", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet"]
+          },
+          "Glimble mobile app": {
+            show: ["accountTpl", , "dataoraevento", "descrizione", "numerotelefonicoapp", "indirizzomailapp"],
+            hide: ["circuitocarta", "panintro", "panprime", "panultime", "panvirtualeintro", "panvirtuale", "wallet", "altrowallet", "linea", "nvettura", "tipoproblemacontactless"]
+          }
+        },
+        circuitocarta: "Select your card’s scheme",
+        circuitocartaOptions: ["--Select--", "Mastercard", "Visa", "American Express", "Maestro", "VPay", "Other"],
+
+        accountTpl: "Operating company",
+        altrocircuito: "Specify your card’s scheme",
+        panintro: "Enter the first and last 4 digits of your card number (PAN)",
+        panprime: "First 4 digits",
+        panultime: "Last 4 digits",
+        panvirtualeintro: "If you use a virtual card (e.g. Apple Pay/Google Pay), enter its 4‑digit PAN",
+        panvirtuale: "Virtual PAN",
+        wallet: "Select your wallet",
+        walletOptions: ["--Select--", "Apple Pay", "Google Pay", "Other"],
+        altrowallet: "Specify your wallet",
+        dataoraevento: "Date and time of the event",
+        linea: "Bus line where the issue occurred or was noticed",
+        nvettura: "Bus registration number",
+        introdescrizione: "Briefly yet clearly describe the issue. Specify what you expected and what happened instead",
+        descrizione: "Description of the issue",
+        tipoluogoproblemacontactless: "Did the issue happen on only one bus or is it recurring?",
+        tipoluogoproblemacontactlessOptions: ["--Select--", "On one bus only", "Always"],
+        indirizzomailapp: "E‑mail associated with your app profile",
+        numerotelefonicoapp: "Phone number associated with your app profile",
+
+        consensoobbligatorio: "CONSENSO OBBLIGATORIO",
+        privacy: "Dichiaro di aver letto e compreso l’informativa sulla privacy di Tpl Fvg, disponibile sul sito www.tplfvg.it e accessibile tramite il link riportato qui sotto",
+        consensofacoltativo: "CONSENSI FACOLTATIVI",
+        indaginidemoscopiche: "Acconsento a essere contattato da Tpl Fvg, anche tramite e-mail, per partecipare a indagini demoscopiche o ricerche di mercato sul livello di soddisfazione e sull’utilizzo dei servizi di trasporto pubblico locale",
+        attivitacommerciali: "Acconsento a ricevere comunicazioni relative a offerte commerciali, promozioni, agevolazioni o sconti su prodotti, servizi e attività di Tpl Fvg o delle aziende consorziate",
+        analisipreferenze: "Acconsento all’analisi delle mie preferenze e abitudini di utilizzo del trasporto pubblico locale, anche mediante elaborazioni elettroniche, al fine di migliorare l’offerta, soddisfare specifiche esigenze, orientare la pianificazione del servizio e ricevere comunicazioni personalizzate da parte di Tpl Fvg o delle aziende consorziate",
+        comunicazionepartner: "Acconsento a ricevere comunicazioni pubblicitarie, promozioni e informazioni commerciali relative a prodotti, servizi e attività di soggetti terzi, partner di Tpl Fvg o delle aziende consorziate",
+        cessioneterzi: "Acconsento alla comunicazione dei miei dati personali a soggetti terzi per finalità commerciali e di marketing",
 
         ultimo: ""
 
       },
       tooltips: {
-
+        accountTpl: "Indica l’azienda sul cui servizio urbano hai rilevato il problema. Se non sai quale azienda indicare, scegli l’opzione TPL FVG",
+        panintro: "Il PAN (Primary Account Number) è il numero da 13 a 19 cifre stampato sulla tua carta di pagamento. Le prime 4 e le ultime 4 cifre servono a identificare la transazione",
+        panvirtualeintro: "Se hai virtualizzato la tua carta in un wallet, entra nel wallet per visualizzare il PAN. Il PAN del wallet ha solo 4 cifre, sempre diverse da quelle della carta fisica",
+        dataoraevento: "Indica la data e l’ora dell’operazione o del momento in cui hai rilevato il problema. Se non conosci l’orario esatto, indica quello approssimativo o il momento in cui ti sei accorto del problema",
+        linea: "Questo campo si attiva dopo aver selezionato l’azienda di riferimento, da cui dipendono le linee proposte. Se come azienda di riferimento hai selezionato TPL FVG, l’elenco includerà tutte le linee urbane operate da Tpl Fvg. Se la segnalazione o l’evento non sono collegati a una linea, puoi lasciare questo campo in bianco",
+        nvettura: "Il numero di matricola dell’autobus è un codice identificativo composto da tre o quattro cifre, generalmente visibile sia all’esterno sia all’interno del mezzo. Il numero di matricola non coincide mai con il numero di linea",
+        numerotelefonicoapp: "Inserisci il numero di telefono che hai usato per attivare l’app o su cui l’app è installata, senza indicare il prefisso internazionale",
         ultimo: ""
 
 
--- intensificazione_traduzioni.js
+++ intensificazione_traduzioni.js
@@ -6,7 +6,9 @@ const resources = {
 
         erroreCampoObbligatorio: "Devi compilare questo campo",
         seleziona: "--Seleziona--",
-        tiposoggetto: "Chi richiede l’intensificazione ",
+        valoreNonValidoDatalist: "Devi selezionare un valore dalla lista",
+        legendaorganizzazione:"DATI DELL’ORGANIZZAZIONE CHE RICHIEDE L’INTENSIFICAZIONE DEL SERVIZIO ",
+        tipoSoggetto: "Chi richiede l’intensificazione ",
         tiposoggettoOptions: ["--Seleziona--",
           "Scuola, università o istituto di formazione",
           "Ente pubblico (non formativo)",
@@ -15,9 +17,26 @@ const resources = {
         ],
         ragioneSociale:"Denominazione dell’organizzazione ",
         nazione:"Sede dell’organizzazione",
-
-        valoreNonValidoDatalist: "Devi selezionare un valore dalla lista",
-
+        indirizzo:"Indirizzo dell’organizzazione",
+        ncivico:"Numero civico",
+        comune:"Comune",
+        citta:"Città",
+        localita:"Località",
+        cap:"CAP",
+        provincia:"Provincia",
+        mailorganizzazione:"E-mail dell’organizzazione",
+        email_confirm_label:"Conferma la mail",
+        
+        numerotelefonicoorganizzazione:"Numero di telefono dell’organizzazione",
+        prefissoorganizzazione:"Prefisso telefonico internazionale organizzazione",
+        
+        legendareferente:"DATI DEL REFERENTE DELL’ORGANIZZAZIONE",
+        nome:"Nome",
+        cognome:"Cognome",
+        mail:"Email",
+        prefissotelefonico:"Prefisso telefonico internazionale",
+        numerotelefonico:"Numero di telefono",
+        legendaserviziorichiesto:"SERVIZIO RICHIESTO",
         tipointensificazione:"Tipo di servizio richiesto",
         tipointensificazioneOptions:["--Seleziona--","Uscita didattica o gita scolastica",
                                     "Intensificazione per eventi sul territorio"],
@@ -32,11 +51,38 @@ const resources = {
           }
         },
         accounttpl:"Territorio e azienda di riferimento",
+        npartecipanti:"Numero di partecipanti previsto",
+        linea_andata:"Linea di andata",
+        andata_partenza_dataora:"Data e ora di partenza dell'andata",
+        andata_luogo_partenza:"Luogo di partenza dell'andata",
+        andata_luogo_destinazione:"Luogo di destinazione dell'andata",
+        linea_ritorno:"Linea di ritorno",
+        ritorno_partenza_dataora:"Data e ora di partenza del ritorno",
+        ritorno_luogo_partenza:"Luogo di partenza del ritorno",
+        ritorno_luogo_destinazione:"Luogo di destinazione del ritorno",
+        nomeevento:"Nome dell'evento",
+        sedeevento:"Sede dell'evento",
+        datainizioevento:"Data di inizio dell'evento",
+        datafineevento:"Data di fine dell'evento",
+        descrizione:"Descrizione",
+        introdescrizione:"Inserisci eventuali ulteriori dettagli per verificare la disponibilità del servizio",
+        consensoobbligatorio: "CONSENSO OBBLIGATORIO",
+          privacy: "Dichiaro di aver letto e compreso l’informativa sulla privacy di Tpl Fvg, disponibile sul sito www.tplfvg.it e accessibile tramite il link riportato qui sotto",
+          consensofacoltativo: "CONSENSI FACOLTATIVI",
+          indaginidemoscopiche: "Acconsento a essere contattato da Tpl Fvg, anche tramite e-mail, per partecipare a indagini demoscopiche o ricerche di mercato sul livello di soddisfazione e sull’utilizzo dei servizi di trasporto pubblico locale",
+          attivitacommerciali: "Acconsento a ricevere comunicazioni relative a offerte commerciali, promozioni, agevolazioni o sconti su prodotti, servizi e attività di Tpl Fvg o delle aziende consorziate",
+          analisipreferenze: "Acconsento all’analisi delle mie preferenze e abitudini di utilizzo del trasporto pubblico locale, anche mediante elaborazioni elettroniche, al fine di migliorare l’offerta, soddisfare specifiche esigenze, orientare la pianificazione del servizio e ricevere comunicazioni personalizzate da parte di Tpl Fvg o delle aziende consorziate",
+          comunicazionepartner: "Acconsento a ricevere comunicazioni pubblicitarie, promozioni e informazioni commerciali relative a prodotti, servizi e attività di soggetti terzi, partner di Tpl Fvg o delle aziende consorziate",
+          cessioneterzi: "Acconsento alla comunicazione dei miei dati personali a soggetti terzi per finalità commerciali e di marketing",
+
 
         ultimo: ""
       },
       tooltips: {
-
+        accounttpl:"Indica il territorio e l’azienda di competenza. Se non sai quale azienda indicare, scegli l’opzione TPL FVG",
+        npartecipanti:"Se ancora non conosci il numero esatto di partecipanti, indica una stima. Se presenti, indicare il numero di partecipanti con disabilità motoria in sedia a rotelle ",
+        linea_andata:"Questo campo si attiva dopo aver selezionato l’area geografica e l’azienda di riferimento, da cui dipendono le linee proposte. Se come azienda di riferimento hai selezionato TPL FVG, l’elenco includerà tutte le linee operate da Tpl Fvg. Se non conosci la linea che opera lungo la direttrice di tuo interesse, puoi lasciare questo campo in bianco",
+        linea_ritorno:"Questo campo si attiva dopo aver selezionato l’area geografica e l’azienda di riferimento, da cui dipendono le linee proposte. Se come azienda di riferimento hai selezionato TPL FVG, l’elenco includerà tutte le linee operate da Tpl Fvg. Se non conosci la linea che opera lungo la direttrice di tuo interesse, puoi lasciare questo campo in bianco",
         ultimo: ""
       }
     }
@@ -48,6 +94,82 @@ const resources = {
         seleziona: "--Select--",
         codiceFiscaleNonValido: "The fiscal/tax code entered is not valid",
         valoreNonValidoDatalist: "You must select a value from list",
+
+
+
+          legendaorganizzazione:"",
+          tipoSoggetto: "",
+          tiposoggettoOptions: ["--Seleziona--",
+            "Scuola, università o istituto di formazione",
+            "Ente pubblico (non formativo)",
+            "Associazione",
+            "Azienda"
+          ],
+          ragioneSociale:"",
+          nazione:"",
+          indirizzo:"",
+          ncivico:"",
+          comune:"",
+          citta:"",
+          localita:"",
+          cap:"",
+          provincia:"",
+          mailorganizzazione:"",
+          email_confirm_label:"Confirm mail",
+
+          numerotelefonicoorganizzazione:"",
+          prefissoorganizzazione:"",
+
+          legendareferente:"",
+          nome:"",
+          cognome:"",
+          mail:"",
+          prefissotelefonico:"",
+          numerotelefonico:"",
+          legendaserviziorichiesto:"",
+          tipointensificazione:"",
+        tipointensificazioneOptions: [
+          "--Select--",
+          "Educational outing or school trip",
+          "Increased service for local events"
+        ],
+          tipointensificazioneOptionsShowHide: {
+              "Educational outing or school trip": {
+              show: [ "linea_andata","npartecipanti","andata_partenza_dataora","andata_luogo_partenza","andata_luogo_destinazione","linea_ritorno","ritorno_partenza_dataora","ritorno_luogo_partenza","ritorno_luogo_destinazione"],
+              hide: ["nomeevento","sedeevento","datainizioevento","datafineevento" ]
+            },
+            "Increased service for local events": {
+              show: ["npartecipanti", "nomeevento","sedeevento","datainizioevento","datafineevento"  ],
+              hide: [ "linea_andata",,"andata_partenza_dataora","andata_luogo_partenza","andata_luogo_destinazione","linea_ritorno","ritorno_partenza_dataora","ritorno_luogo_partenza","ritorno_luogo_destinazione"]
+            }
+          },
+          accounttpl:"",
+          npartecipanti:"",
+          linea_andata:"",
+          andata_partenza_dataora:"",
+          andata_luogo_partenza:"",
+          andata_luogo_destinazione:"",
+          linea_ritorno:"",
+          ritorno_partenza_dataora:"",
+          ritorno_luogo_partenza:"",
+          ritorno_luogo_destinazione:"",
+          nomeevento:"",
+          sedeevento:"",
+          datainizioevento:"",
+          datafineevento:"",
+          descrizione:"",
+
+          consensoobbligatorio: "CONSENSO OBBLIGATORIO",
+            privacy: "Dichiaro di aver letto e compreso l’informativa sulla privacy di Tpl Fvg, disponibile sul sito www.tplfvg.it e accessibile tramite il link riportato qui sotto",
+            consensofacoltativo: "CONSENSI FACOLTATIVI",
+            indaginidemoscopiche: "Acconsento a essere contattato da Tpl Fvg, anche tramite e-mail, per partecipare a indagini demoscopiche o ricerche di mercato sul livello di soddisfazione e sull’utilizzo dei servizi di trasporto pubblico locale",
+            attivitacommerciali: "Acconsento a ricevere comunicazioni relative a offerte commerciali, promozioni, agevolazioni o sconti su prodotti, servizi e attività di Tpl Fvg o delle aziende consorziate",
+            analisipreferenze: "Acconsento all’analisi delle mie preferenze e abitudini di utilizzo del trasporto pubblico locale, anche mediante elaborazioni elettroniche, al fine di migliorare l’offerta, soddisfare specifiche esigenze, orientare la pianificazione del servizio e ricevere comunicazioni personalizzate da parte di Tpl Fvg o delle aziende consorziate",
+            comunicazionepartner: "Acconsento a ricevere comunicazioni pubblicitarie, promozioni e informazioni commerciali relative a prodotti, servizi e attività di soggetti terzi, partner di Tpl Fvg o delle aziende consorziate",
+            cessioneterzi: "Acconsento alla comunicazione dei miei dati personali a soggetti terzi per finalità commerciali e di marketing",
+
+
+        
 
         ultimo: ""
 
