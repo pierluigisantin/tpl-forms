318798279016559489d14288ee7a658f9868d798
Pierluigi Santin <p.santin@e-deimos.it>
2025-06-18 15:58:52 +0000

    traduzioni michele

--- reclamo_traduzioni.js
+++ reclamo_traduzioni.js
@@ -13,15 +13,14 @@ const resources = {
         nome: "Nome",
         cognome: "Cognome",
         nazione: "Nazione di residenza",
-        ncivico:"Street N°",
         indirizzo: "Indirizzo di residenza o domicilio",
-
+        ncivico:"Numero civico",
         comune: "Comune",
-
+        localita:"Località o frazione",
         prefissotelefonico:"Prefisso telefonico internazionale",
         numerotelefonico:"Numero di cellulare",
         mail: "E-mail",
-
+        email_confirm_label:"ripeti e-mail",
         abbonatosino:"Abbonato",
         numerotesserinoidentificativo: `Numero di tesserino identificativo Tpl Fvg (se il reclamante ne è titolare)`,
 
@@ -45,16 +44,16 @@ const resources = {
 
 
         descrizione: "Descrizione dell'evento",
-        introdescrizione:"Descrivi l’evento o il problema in modo sintetico ma completo, fornendo tutte le informazioni utili per la ricostruzione dei fatti. Se il reclamo viene presentato da una persona diversa da chi ha subito il disservizio, indica anche i dati anagrafici di quest’ultimo. Se l’evento è avvenuto presso una biglietteria, una rivendita o in un altro contesto specifico, indicalo in questa descrizione. Se desideri allegare immagini, titoli di viaggio o altri documenti utili, potrai farlo rispondendo alla e-mail di notifica che riceverai dopo aver compilato questo form.",
+        introdescrizione:"Descrivi l’evento o il problema in modo sintetico ma completo, includendo tutte le informazioni utili per ricostruire quanto successo. Se presenti il reclamo per conto di un’altra persona, indica anche i dati di quest'ultima. Potrai allegare immagini, titoli di viaggio o altri documenti rispondendo all’e-mail di conferma che riceverai dopo aver inviato questo modulo.",
 
         consensoobbligatorio:"CONSENSO OBBLIGATORIO",
-        privacy: "Dichiaro di aver letto e compreso l’informativa sulla privacy di Tpl Fvg, disponibile sul sito www.tplfvg.it e accessibile tramite il link riportato qui sotto",
+        privacy: "Dichiaro di aver letto e compreso l’informativa sulla privacy di Tpl Fvg, disponibile sul sito www.tplfvg.it e accessibile tramite il link riportato qui sotto.",
         consensofacoltativo:"CONSENSI FACOLTATIVI",
-        indaginidemoscopiche:"Acconsento a essere contattato da Tpl Fvg, anche tramite e-mail, per partecipare a indagini demoscopiche o ricerche di mercato sul livello di soddisfazione e sull’utilizzo dei servizi di trasporto pubblico locale",
-        attivitacommerciali:"Acconsento a ricevere comunicazioni relative a offerte commerciali, promozioni, agevolazioni o sconti su prodotti, servizi e attività di Tpl Fvg o delle aziende consorziate",
-        analisipreferenze:"Acconsento all’analisi delle mie preferenze e abitudini di utilizzo del trasporto pubblico locale, anche mediante elaborazioni elettroniche, al fine di migliorare l’offerta, soddisfare specifiche esigenze, orientare la pianificazione del servizio e ricevere comunicazioni personalizzate da parte di Tpl Fvg o delle aziende consorziate",
-        comunicazionepartner:"Acconsento a ricevere comunicazioni pubblicitarie, promozioni e informazioni commerciali relative a prodotti, servizi e attività di soggetti terzi, partner di Tpl Fvg o delle aziende consorziate",
-        cessioneterzi:"Acconsento alla comunicazione dei miei dati personali a soggetti terzi per finalità commerciali e di marketing",
+        indaginidemoscopiche:"Acconsento a essere contattato da Tpl Fvg per partecipare a indagini demoscopiche o ricerche di mercato sul servizio di trasporto pubblico locale.",
+        attivitacommerciali:"Acconsento a ricevere comunicazioni relative a offerte e agevolazioni sui servizi di Tpl Fvg e delle aziende consorziate.",
+        analisipreferenze:"Acconsento all’analisi delle mie preferenze e abitudini di utilizzo del trasporto pubblico locale, anche mediante elaborazioni elettroniche, al fine di migliorare l’offerta e ricevere comunicazioni personalizzate da parte di Tpl Fvg.",
+        comunicazionepartner:"Acconsento a ricevere comunicazioni pubblicitarie e informazioni commerciali relative a prodotti o servizi di soggetti terzi, partner di Tpl Fvg o delle aziende consorziate.",
+        cessioneterzi:"Acconsento alla comunicazione dei miei dati personali a soggetti terzi per finalità commerciali e di marketing.",
         
 
        invia:"Invia reclamo"
@@ -68,9 +67,14 @@ const resources = {
 
 
         tipoSoggetto: "Indica se stai presentando un reclamo a titolo personale o se lo fai per conto di un’associazione, un’azienda o un ente pubblico.",
-
+        nome: "Anche se il reclamo è presentato per conto di un ente, un'associazione o un'azienda, è necessario indicare i dati del referente che compila questo modulo.",
+        cognome: "Anche se il reclamo è presentato per conto di un ente, un'associazione o un'azienda, è necessario indicare i dati del referente che compila questo modulo.",
+        nazione: "Scrivi il nome del tuo Paese di residenza: è sufficiente digitarne le prime lettere e poi selezionare il nome corretto dall’elenco proposto. Il campo è impostato di default su ITALIA.",
+        indirizzo: "Inserisci un indirizzo presso cui desideri ricevere eventuali comunicazioni via posta ordinaria.",
+        comune: "Seleziona un Comune e il relativo CAP tra quelli disponibili nel menù.",
+        prefissotelefonico:"Seleziona il prefisso internazionale tra quelli disponibili nel menù.",
         numerotesserinoidentificativo:`<div>
-              È il numero di sette cifre che identifica in modo univoco il cliente abbonato: si trova sia sul tesserino identificativo, sia sugli abbonamenti acquistati utilizzando quel tesserino.<br/>
+              È il numero di sette cifre che identifica in modo univoco il cliente abbonato: si trova sia sul tesserino identificativo sia sugli abbonamenti acquistati utilizzando quel tesserino.<br/><br/>
               <img src="immagine_tooltip_abbonamenti.png" alt="Tesserino esempio"style="max-width: 100%; height: auto;"/>
             </div>`,
 
@@ -89,7 +93,7 @@ const resources = {
     translation: {
       labels: {
         legendanagrafica:"PERSONAL DETAILS OF THE PERSON SUBMITTING THE COMPLAINT",
-        valoreNonValidoDatalist:"You must selecta a value from list",
+        valoreNonValidoDatalist:"You must select a value from list",
         erroreCampoObbligatorio: "You must fill in this field",
         seleziona: "--Select--",
 
@@ -99,15 +103,17 @@ const resources = {
         nome: "First Name",
         cognome: "Last Name",
         nazione: "Country",
-        ncivico:"Street N°",
+        ncivico:"Street number",
         indirizzo: "Residential or mailing address",
-
-        comune: "Municipality",
-
+        citta:"City",
+        comune:"Municipality",
+        localita:"Local area",
+        cap:"Postal code",
+        provincia:"Province / State / Region",
         prefissotelefonico:"International Phone Prefix",
         numerotelefonico:"Mobile Phone Number",
         mail: "E-mail",
-
+        email_confirm_label:"Confirm email",
         abbonatosino:"Pass holder",
         numerotesserinoidentificativo:"Tpl Fvg Identification card number (if the complainant holds one)",
 
@@ -131,16 +137,16 @@ const resources = {
 
 
         descrizione: "Description of the event",
-        introdescrizione:"Briefly but clearly describe the event or issue, providing all the information needed to reconstruct what happened. If the complaint is being submitted by someone other than the person who experienced the issue, please also include that person’s personal details. If the event took place at a ticket office, a sales point, or in another specific setting, mention it in your description. If you wish to attach images, travel tickets, or other relevant documents, you can do so by replying to the notification e-mail you will receive after completing this form.",
+        introdescrizione:"Briefly but clearly describe the event or issue, including all the information needed to understand what happened. If you are submitting the complaint on behalf of someone else, please include their details as well. You will be able to attach images, tickets, or other relevant documents by replying to the confirmation e-mail you will receive after submitting this form.",
 
-        consensoobbligatorio:"MANDATORY CONSENT",
-        privacy: "I declare that I have read and understood the Tpl Fvg privacy policy, available on the website www.tplfvg.it and accessible via the link provided below",
-        consensofacoltativo:"OPTIONAL CONSENTS",
-        indaginidemoscopiche:"I consent to being contacted by Tpl Fvg, including via email, to participate in surveys or market research regarding customer satisfaction and the use of public transport services",
-        attivitacommerciali:"I consent to receiving communications  regarding commercial offers, promotions, discounts, or special deals related to the products, services, and activities of Tpl Fvg or the member companies of the Tpl Fvg consortium",
-        analisipreferenze:"I consent to the analysis of my preferences and habits in the use of public transport services, including through electronic processing, in order to improve the service offering, meet specific needs, support service planning, and receive personalized communications from Tpl Fvg or the member companies of the Tpl Fvg consortium",
-        comunicazionepartner:"I consent to receiving advertising, promotional content, and commercial information regarding products, services, and activities of third-party entities, partners of Tpl Fvg or the member companies of the Tpl Fvg consortium",
-        cessioneterzi:"I consent to the sharing of my personal data with third parties for commercial and marketing purpose",
+        consensoobbligatorio: "MANDATORY CONSENT",
+        privacy: "I declare that I have read and understood the Tpl Fvg privacy policy, available on the website www.tplfvg.it and accessible via the link below.",
+        consensofacoltativo: "OPTIONAL CONSENTS",
+        indaginidemoscopiche: "I agree to be contacted by Tpl Fvg to take part in surveys or market research about local public transport services.",
+        attivitacommerciali: "I agree to receive information about offers and discounts on services provided by Tpl Fvg and the companies that are part of the consortium.",
+        analisipreferenze: "I agree to the analysis of my preferences and habits in using local public transport, including electronic processing, to improve the service and receive personalised messages from Tpl Fvg.",
+        comunicazionepartner: "I agree to receive advertising and commercial information about products or services from third parties, partners of Tpl Fvg or of the consortium companies.",
+        cessioneterzi: "I agree to the sharing of my personal data with third parties for commercial and marketing purposes.",
 
 
        invia:"Submit complain"
@@ -153,18 +159,25 @@ const resources = {
 
 
 
-        tipoSoggetto: "Indicate whether you are submitting the complaint on your own behalf or on behalf of an association, a company, or a public institution",
-        numerotesserinoidentificativo:`<div>
-            It is the seven-digit number that uniquely identifies the pass holder. It appears both on the Tpl Fvg identification card and on all passes purchased using that card.<br/>
-            <img src="immagine_tooltip_abbonamenti.png" alt="Tesserino esempio"style="max-width: 100%; height: auto;"/>
-          </div>`,
-        accountTpl:"Select the company responsible for the area concerned. If you are unsure which company to choose, or if the event is not clearly linked to one of the consortium’s companies, select the TPL FVG option",
-        servizio:"Indicate the service related to the event. If the event does not concern any of the listed services, or cannot be clearly linked to one of them, select the OTHER ASPECTS OF THE SERVICE option. If you are unsure about the type of service, you may leave this field blank",
-        linea: "This field becomes active after selecting the reference company. The available lines depend on the selected company. If you selected TPL FVG as the reference company, the list will include all lines operated by Tpl Fvg in Friuli Venezia Giulia and surrounding areas. If the event or report is not related to a specific line, you may leave this field blank",
-        fermata: "If you consider it useful, indicate the stop where the event occurred. To view the list of stops, you must first select a line in the previous field. Only the stops served by the selected line will be displayed. If the stop is served by multiple lines, you may select any of them. If the report or event does not concern a specific stop, you can leave this field blank",
-        dataoraevento:"Enter the date and time of the event. If the issue is not linked to a single episode - such as a malfunctioning device or an anomaly on the website or one of our apps - please indicate the date and time when you noticed the problem",
-        frazioneevento:"If possible, indicate the locality where the event occurred. Knowing the exact place is often very important to carry out the necessary checks and respond more effectively",
-        nvettura:"The fleet number of the bus is a three or four-digit identification code, usually visible both outside and inside the vehicle. The fleet number never matches the route number",
+        tipoSoggetto: "Indicate whether you are submitting the complaint on your own behalf or on behalf of an association, a company, or a public body.",
+        nome: "Even if the complaint is submitted on behalf of a body, an association, or a company, you must provide the details of the contact person filling out this form.",
+        cognome: "Even if the complaint is submitted on behalf of a body, an association, or a company, you must provide the details of the contact person filling out this form.",
+        nazione: "Enter the name of your country of residence: just type the first few letters and then select the correct name from the suggested list. The default value is ITALY.",
+        indirizzo: "Enter an address where you wish to receive any communications by regular mail.",
+        comune: "Select a municipality and its corresponding postal code from the dropdown menu.",
+        prefissotelefonico: "Select the international dialing code from the dropdown menu.",
+        numerotesserinoidentificativo: `<div>
+          This is the seven-digit number that uniquely identifies the pass holder. It can be found both on the ID card and on the passes purchased using that card.<br/><br/>
+          <img src="immagine_tooltip_abbonamenti.png" alt="Sample ID card" style="max-width: 100%; height: auto;"/>
+        </div>`,
+
+        accountTpl: "Select the company responsible for the area. If you are unsure which company to choose, or if the event is not attributable to a single consortium company, select the TPL FVG option.",
+        servizio: "Specify the service the event is related to. If it does not concern any of the listed services, or cannot be directly attributed to one of them, select the OTHER ASPECTS OF THE SERVICE option. If you do not know the type of service, you can leave this field blank.",
+        linea: "This field becomes active after selecting the relevant company, as the list of available lines depends on that choice. If you select TPL FVG as the company, the list will include all routes operated by Tpl Fvg in Friuli Venezia Giulia and neighbouring areas. If the event or report is not related to a specific route, you may leave this field blank.",
+        fermata: "Indicate the bus stop where the event occurred, if you think it is useful. To see the list of stops, you must first select a route in the previous field. Only stops served by the selected route will be shown. If the stop is served by several routes, you may select any of them. If the report or event does not involve a specific stop, you may leave this field blank.",
+        dataoraevento: "Indicate the date and time of the event. If the problem is not linked to a single episode (for example, a malfunctioning device or an issue with our website or app), enter the date and time when you noticed the issue.",
+        frazioneevento: "If possible, indicate the locality or hamlet where the event occurred. Knowing the exact place is often very important to carry out the necessary checks and respond more effectively.",
+        nvettura: "The vehicle number is a three- or four-digit identification code, usually visible both inside and outside the bus. This number is different from the route number.",
         descrizione:""
       }
     }
